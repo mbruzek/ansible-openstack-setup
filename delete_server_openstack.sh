2d86c40347ca461e5fa881e0d90a8ec964a6962c
Matt Bruzek <mbruzek@gmail.com>
2017-09-13 08:14:30 -0500

    Making the clean up much more specific

--- delete_server_openstack.sh
+++ delete_server_openstack.sh
@@ -7,56 +7,48 @@ set -eu
 # Load the variables for the values to clean up.
 source variables
 source $openstack_rc_path
+# Path to the openstack client.
+openstack=virtualenv/bin/openstack
 
-server_ids=$(virtualenv/bin/openstack server list --format value -c ID)
-echo "Deleting all servers"
-for server_id in ${server_ids}; do
-  virtualenv/bin/openstack server delete ${server_id}
-done
+echo "Deleting the ${server_name} server"
+${openstack} server delete ${server_name}
 
-keypair_names=$(virtualenv/bin/openstack keypair list --format value -c Name)
-echo "Deleting all keypairs"
-for keypair_name in ${keypair_names}; do
-  virtualenv/bin/openstack keypair delete ${keypair_name}
-done
+echo "Deleting the ${keypair_name} keypair"
+${openstack} keypair delete ${keypair_name}
 
-flavor_id=$(virtualenv/bin/openstack flavor show ${server_flavor} --format value -c id)
-echo "Deleting flavor"
-virtualenv/bin/openstack flavor delete ${flavor_id}
+echo "Deleting ${server_flavor} flavor"
+${openstack} flavor delete ${server_flavor}
 
-floating_ips=$(virtualenv/bin/openstack floating ip list --format value -c ID)
+floating_ips=$(${openstack} floating ip list --format value -c ID)
 echo "Deleting all floating ips"
 for floating_id in ${floating_ips}; do
-  virtualenv/bin/openstack floating ip delete ${floating_id}
+  ${openstack} floating ip delete ${floating_id}
 done
 
 # Check for the security group before attempting delete.
-if virtualenv/bin/openstack security group show ${security_group_name}; then
+if ${openstack} security group show ${security_group_name}; then
   echo "Deleting the ${security_group_name} that has all rules"
-  virtualenv/bin/openstack security group delete ${security_group_name}
+  ${openstack} security group delete ${security_group_name}
 fi
 
 # Check for the subnet before attempting to delete it.
-if virtualenv/bin/openstack subnet show ${subnet_name}; then
+if ${openstack} subnet show ${subnet_name}; then
   # The subnet can not be deleted until it is removed from the router.
   echo "Remove the subnet ${subnet_name} from the router ${router_name}"
-  virtualenv/bin/openstack router remove subnet ${router_name} ${subnet_name} || true
+  ${openstack} router remove subnet ${router_name} ${subnet_name} || true
 
   echo "Deleting the subnet ${subnet_name}"
-  virtualenv/bin/openstack subnet delete ${subnet_name}
+  ${openstack} subnet delete ${subnet_name}
 fi
 
 # Check for the network before attempting to delete it.
-if virtualenv/bin/openstack network show ${network_name}; then
+if ${openstack} network show ${network_name}; then
   echo "Deleting the network ${network_name}"
-  networks=$(virtualenv/bin/openstack network show ${network_name} --format value -c id)
+  networks=$(${openstack} network show ${network_name} --format value -c id)
   for network_id in ${networks}; do
-    virtualenv/bin/openstack network delete ${network_id}
+    ${openstack} network delete ${network_id}
   done
 fi
 
-routers=$(virtualenv/bin/openstack router list --format value -c ID)
-echo "Deleting all routers"
-for router in ${routers}; do
-  virtualenv/bin/openstack router delete ${router}
-done
+echo "Deleting the ${router_name} router"
+${openstack} router delete ${router_name}
