6c93374162d1b67dd2e2868d1d15f75a4dd5ecfe
Matt Bruzek <mbruzek@gmail.com>
2017-10-04 15:41:14 -0500

    Adding a script that shows status on the OpenStack objects that are configured.

---
+++ openstack_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A script to show the status on all the OpenStack objects that are configured.
+openstack server list
+openstack router list
+openstack network list
+openstack subnet list
+openstack keypair list
+openstack floating ip list
+openstack security group list
+openstack flavor list
+openstack image list
+openstack project list
+openstack stack list
